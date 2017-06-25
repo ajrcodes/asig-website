3dc47c289999105d9e53e45df548f664dae376b8
Ryan Donovan <rad9vy@virginia.edu>
2017-06-25 12:30:07 -0400

    Started on Current Leadership template

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/ZYChapterHistory', views.ZYChapterHistory, name='ZYChapterHistory'),
     url(r'^about/NationalsHistory', views.NationalsHistory, name='NationalsHistory'),
+    url(r'^about/CurrentLeadership', views.CurrentLeadership, name='CurrentLeadership'),
 
 ]
 
