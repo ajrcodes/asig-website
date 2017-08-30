6c2fb9e5fadfe434a22d2d08f9f47f8b122fe837
ANDREW ROBERTS <ajrcodes@gmail.com>
2017-08-29 21:09:45 -0400

    Refactored code

--- core/urls.py
+++ core/urls.py
@@ -4,9 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/ZYChapterHistory', views.ZYChapterHistory, name='ZYChapterHistory'),
-    url(r'^about/NationalsHistory', views.NationalsHistory, name='NationalsHistory'),
-    url(r'^about/CurrentLeadership', views.CurrentLeadership, name='CurrentLeadership'),
+    url(r'^about/zy-chapter-history', views.ZYChapterHistory, name='ZYChapterHistory'),
+    url(r'^about/nationals-history', views.NationalsHistory, name='NationalsHistory'),
+    url(r'^about/current-leadership', views.CurrentLeadership, name='CurrentLeadership'),
 
 ]
-
